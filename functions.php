0bed081dfdbe48336f0cebdc6e3a54c04ae4e5a3
Scott Lyttle <scott.lyttle@gmail.com>
2013-09-05 11:14:16 +1000

    Initial Commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+// enqueue scripts and styles
+include('functions/scripts.php');
+
+// setup and cleanup
+include('functions/setup.php');
+
+// sidebars
+include('functions/sidebars.php');
+
+// custom comment layout
+include('functions/comments.php');
+
+// additional functionality
+include('functions/misc.php');
